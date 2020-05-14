9fc966cc6a6f349cc42fd78e3fce5fc6ffcf9614
unknown <mehedi5051@gmail.com>
2020-05-14 16:32:25 +0600

    using react player to play youtube videos

---
+++ src/components/Player.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactPlayer from "react-player";
+
+const Player = ({ videoId }) => {
+	return (
+		<ReactPlayer url={`https://www.youtube.com/watch?v=${videoId}`} controls />
+	);
+};
+
+export default Player;
