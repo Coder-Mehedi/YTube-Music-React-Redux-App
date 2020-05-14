f0d58cc97279e2a35ce76a8a9fb4dbfabe458a2e
unknown <mehedi5051@gmail.com>
2020-05-14 21:43:37 +0600

    lyrics reducer added to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import youtubeReducer from "./youtubeReducer";
+import lyricsReducer from "./lyricsReducer";
 
 export default combineReducers({
 	youtube: youtubeReducer,
+	lyrics: lyricsReducer,
 });
