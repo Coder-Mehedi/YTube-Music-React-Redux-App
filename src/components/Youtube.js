8702ae31ee980c500cc3e0c139f821bf050ed11d
unknown <mehedi5051@gmail.com>
2020-05-14 16:26:02 +0600

    getting searchResult using redux and mapping

---
+++ src/components/Youtube.js
@@ -0,0 +1,30 @@
+import React, { Fragment } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import Player from "./Player";
+import SearchForm from "./SearchForm";
+
+const Youtube = () => {
+	const searchResults = useSelector((state) => state.youtube.searchResults);
+
+	return (
+		<div>
+			<h2 className="title">YTube Music</h2>
+			<SearchForm />
+			<ul>
+				{searchResults.map((searchResult) => (
+					<Fragment>
+						<li>
+							<img src={searchResult.snippet.thumbnails.default.url} alt="" />
+
+							<h3>{searchResult.snippet.title}</h3>
+						</li>
+
+						{/* <Player videoId={searchResult.id.videoId} /> */}
+					</Fragment>
+				))}
+			</ul>
+		</div>
+	);
+};
+
+export default Youtube;
