9d0de783ae062189dcd5c353f9364611141df157
unknown <mehedi5051@gmail.com>
2020-05-14 21:40:40 +0600

    using redux to change current video id and modal state

--- src/components/Youtube.js
+++ src/components/Youtube.js
@@ -2,9 +2,21 @@ import React, { Fragment } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import Player from "./Player";
 import SearchForm from "./SearchForm";
+import {
+	setModalOpenAction,
+	setModalCloseAction,
+} from "../actions/youtubeAction";
+import Modal from "./modal/Modal";
 
 const Youtube = () => {
 	const searchResults = useSelector((state) => state.youtube.searchResults);
+	const dispatch = useDispatch();
+
+	const setModalOpen = (videoId) => dispatch(setModalOpenAction(videoId));
+
+	const playMusic = (videoId) => {
+		setModalOpen(videoId);
+	};
 
 	return (
 		<div>
@@ -17,13 +29,17 @@ const Youtube = () => {
 							<img src={searchResult.snippet.thumbnails.default.url} alt="" />
 
 							<h3>{searchResult.snippet.title}</h3>
-							<i class="fas fa-play-circle"></i>
+							<i
+								className="fas fa-play-circle"
+								onClick={() => playMusic(searchResult.id.videoId)}
+							></i>
 						</li>
 
 						{/* <Player videoId={searchResult.id.videoId} /> */}
 					</Fragment>
 				))}
 			</ul>
+			<Modal />
 		</div>
 	);
 };
