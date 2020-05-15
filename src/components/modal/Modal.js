e01b6018a7637c201404e17d65f3665fa566652c
unknown <mehedi5051@gmail.com>
2020-05-15 12:31:12 +0600

    modification of modal ui

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -7,24 +7,28 @@ import { setModalCloseAction } from "../../actions/youtubeAction";
 const Modal = () => {
 	const modalOpen = useSelector((state) => state.youtube.modalOpen);
 	const videoId = useSelector((state) => state.youtube.currentVideoId);
+	const lyrics = useSelector((state) => state.lyrics.lyrics);
 	const dispatch = useDispatch();
 	const setModalClose = () => dispatch(setModalCloseAction());
 
 	return (
 		<div>
-			<div
-				id="myModal"
-				className={`${styles.modal} ${modalOpen && styles.open}`}
-			>
-				<div className={styles.modalContent}>
-					<div className={styles.modalHeader}>
-						<span className={styles.close} onClick={setModalClose}>
-							&times;
-						</span>
-					</div>
-					<div className={styles.modalBody}>
-						<Player videoId={videoId} />
-					</div>
+			<div className={`${styles.modal} ${modalOpen && styles.open}`}>
+				<div className={styles.modalHeader}>
+					<h3>
+						YTube Music <i className="fas fa-play"></i>
+					</h3>
+					<span className={styles.close} onClick={setModalClose}>
+						&times;
+					</span>
+				</div>
+				<div className={styles.modalBody}>
+					<Player videoId={videoId} />
+					{lyrics ? (
+						<p>{lyrics}</p>
+					) : (
+						<h2 className="noLyrics">Sorry No Lyrics Found</h2>
+					)}
 				</div>
 			</div>
 		</div>
