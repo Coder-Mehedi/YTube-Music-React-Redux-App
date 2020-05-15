1f7e8822ac497bd3b814df4b1b65f7abe72a543d
unknown <mehedi5051@gmail.com>
2020-05-15 14:12:07 +0600

    hide popular videos after search

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import "./App.css";
 import Youtube from "./components/Youtube";
 import PopularVideos from "./components/PopularVideos";
 
 function App() {
+	const searchResults = useSelector((state) => state.youtube.searchResults);
+
 	return (
 		<div className="App">
 			<Youtube />
-			<PopularVideos />
+			{searchResults.length === 0 && <PopularVideos />}
 		</div>
 	);
 }
