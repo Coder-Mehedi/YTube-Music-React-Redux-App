5416cc26bc6bb6f19925f6c3dafecf2f99952600
unknown <mehedi5051@gmail.com>
2020-05-15 13:56:57 +0600

    added popular video component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Youtube from "./components/Youtube";
+import PopularVideos from "./components/PopularVideos";
 
 function App() {
 	return (
 		<div className="App">
 			<Youtube />
+			<PopularVideos />
 		</div>
 	);
 }
