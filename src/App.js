7599d494347174bdb36192b0ec5f424287adcb87
unknown <mehedi5051@gmail.com>
2020-05-15 17:59:09 +0600

    main title and searchform rendering from App component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import { useSelector } from "react-redux";
 import "./App.css";
 import Youtube from "./components/Youtube";
 import PopularVideos from "./components/PopularVideos";
+import SearchForm from "./components/search/SearchForm";
 
 function App() {
 	const searchResults = useSelector((state) => state.youtube.searchResults);
 
 	return (
 		<div className="App">
+			<h2 className="title">YTube Music</h2>
+			<SearchForm />
 			<Youtube />
 			{searchResults.length === 0 && <PopularVideos />}
 		</div>
