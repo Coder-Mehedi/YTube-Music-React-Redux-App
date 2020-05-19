4e7eb70eba1eecf7f9b8dc105d976181a62dd549
unknown <mehedi5051@gmail.com>
2020-05-19 22:30:59 +0600

    popular video dispatching done here

--- src/components/PopularVideos.js
+++ src/components/PopularVideos.js
@@ -1,8 +1,16 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import Card from "./Card/Card";
+import { getYoutubePopularMusicAction } from "../actions/youtubeAction";
 
 const PopularVideos = () => {
+	const dispatch = useDispatch();
+	const getYoutubePopularMusic = () => dispatch(getYoutubePopularMusicAction());
+	useEffect(() => {
+		getYoutubePopularMusic();
+		// eslint-disable-next-line
+	}, []);
+
 	const popularMusic = useSelector((state) => state.youtube.popularMusic);
 
 	return (
