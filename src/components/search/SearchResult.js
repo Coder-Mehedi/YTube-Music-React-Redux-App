e90e3d4fe74497d9aca393b8bcef879b0c7ed6cb
unknown <mehedi5051@gmail.com>
2020-05-19 22:35:34 +0600

    search result component cleaned because of modal removed

--- src/components/search/SearchResult.js
+++ src/components/search/SearchResult.js
@@ -1,39 +1,16 @@
 import React from "react";
-import { useDispatch } from "react-redux";
-import { setModalOpenAction } from "../../actions/youtubeAction";
-import { getLyricsAction } from "../../actions/lyricsAction";
+import { useSelector } from "react-redux";
+import Card from "../Card/Card";
 
-const SearchResult = ({
-	searchResult: {
-		id: { videoId },
-		snippet: { thumbnails, title },
-	},
-}) => {
-	const dispatch = useDispatch();
-	const setModalOpen = (videoId) => dispatch(setModalOpenAction(videoId));
-	const getLyrics = (searchQuery) => dispatch(getLyricsAction(searchQuery));
-
-	const playMusic = (videoId) => {
-		setModalOpen(videoId);
-		const artist = title.split("-")[0];
-		const splittedTitle = title.split("-")[1];
-		getLyrics({ artist, title: splittedTitle });
-	};
+const SearchResult = () => {
+	const searchResults = useSelector((state) => state.youtube.searchResults);
 
 	return (
-		<li key={videoId}>
-			<img
-				src={thumbnails.high.url}
-				alt=""
-				className="clickable"
-				onClick={() => playMusic(videoId)}
-			/>
-
-			<h3 className="clickable" onClick={() => playMusic(videoId)}>
-				{title}
-			</h3>
-			<i className="fas fa-play-circle" onClick={() => playMusic(videoId)}></i>
-		</li>
+		<div className="cardContainer">
+			{searchResults.map((music) => (
+				<Card music={music} key={music.id} searched={true} />
+			))}
+		</div>
 	);
 };
 
