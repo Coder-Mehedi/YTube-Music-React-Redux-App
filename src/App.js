a9d82b3a758d212f78b6e5697ea2c2d90c5685dc
unknown <mehedi5051@gmail.com>
2020-05-25 20:59:23 +0600

    setup graphql client and auth route

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React from "react";
 import "./App.css";
 import MenuBar from "./components/Menubar";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollo-boost";
 import Video from "./components/Video";
 import Home from "./components/Home";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import AuthRoute from "./utils/AuthRoute";
+
+const client = new ApolloClient({
+	uri: "http://localhost:4000",
+});
 
 function App() {
 	return (
-		<div className="App">
-			<Router>
-				<MenuBar />
-				<Route exact path="/" component={Home} />
-				<Route exact path="/:videoId" component={Video} />
-			</Router>
-		</div>
+		<ApolloProvider client={client}>
+			<div className="App">
+				<Router>
+					<MenuBar />
+					<Switch>
+						<Route exact path="/" component={Home} />
+						<AuthRoute path="/login" component={Login} />
+						<AuthRoute path="/register" component={Register} />
+						<Route path="/:videoId" component={Video} />
+					</Switch>
+				</Router>
+			</div>
+		</ApolloProvider>
 	);
 }
 
