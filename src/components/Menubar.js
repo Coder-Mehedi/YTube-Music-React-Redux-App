c3e0a8b287501b5721de990a964a42293d52cebd
unknown <mehedi5051@gmail.com>
2020-05-25 21:02:38 +0600

    showing dynamic/conditional navbar links

--- src/components/Menubar.js
+++ src/components/Menubar.js
@@ -1,7 +1,29 @@
-import React, { div } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { logoutAction } from "../actions/authAction";
 
 const Menubar = () => {
+	const user = useSelector((state) => state.auth.user);
+	const dispatch = useDispatch();
+	const logout = () => dispatch(logoutAction());
+
+	const menuBar = user ? (
+		<li>
+			<button className="logout-btn" onClick={logout}>
+				Logout
+			</button>
+		</li>
+	) : (
+		<>
+			<li>
+				<Link to="/login">Login</Link>
+			</li>
+			<li>
+				<Link to="/register">Register</Link>
+			</li>
+		</>
+	);
 	return (
 		<div className="menubar">
 			<h2 className="title">YTube Music</h2>
@@ -9,12 +31,7 @@ const Menubar = () => {
 				<li>
 					<Link to="/">Home</Link>
 				</li>
-				<li>
-					<Link to="/login">Login</Link>
-				</li>
-				<li>
-					<Link to="/register">Register</Link>
-				</li>
+				{menuBar}
 			</ul>
 		</div>
 	);
