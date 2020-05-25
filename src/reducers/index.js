ef3ba79f1dca7d78c7e5fc4d39e3c8a1ca947e1d
unknown <mehedi5051@gmail.com>
2020-05-25 21:06:05 +0600

    auth reducer added to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import youtubeReducer from "./youtubeReducer";
 import lyricsReducer from "./lyricsReducer";
+import authReducer from "./authReducer";
 
 export default combineReducers({
 	youtube: youtubeReducer,
 	lyrics: lyricsReducer,
+	auth: authReducer,
 });
