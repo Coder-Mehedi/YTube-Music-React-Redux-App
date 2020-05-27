86a6cbd663a0467c5de31e32cb1867265321998d
unknown <mehedi5051@gmail.com>
2020-05-27 12:47:21 +0600

    click on any video will add to users watched list

--- src/components/Video.js
+++ src/components/Video.js
@@ -2,17 +2,31 @@ import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import ReactPlayer from "react-player";
 import { getLyricsAction } from "../actions/lyricsAction";
+import gql from "graphql-tag";
+import { useMutation } from "react-apollo";
 
 const Player = (props) => {
+	console.log(props.location.state.thumbnail);
 	const title = props.location.state.title;
 	const dispatch = useDispatch();
 	const getLyrics = (searchQuery) => dispatch(getLyricsAction(searchQuery));
+	const [addWatch] = useMutation(ADD_WATCH, {
+		variables: {
+			videoId: props.match.params.videoId,
+			title,
+			thumbnail: props.location.state.thumbnail,
+		},
+		onError(err) {
+			// setErrors(err.graphQLErrors[0].extensions.exception.errors);
+			console.log(err.graphQLErrors[0]);
+		},
+	});
 
 	useEffect(() => {
 		getLyrics(title);
+		addWatch();
 		// eslint-disable-next-line
-	}, []);
-	console.log(props.match.params.videoId);
+	}, [title]);
 
 	const lyrics = useSelector((state) => state.lyrics.lyrics);
 	return (
@@ -28,3 +42,13 @@ const Player = (props) => {
 };
 
 export default Player;
+
+const ADD_WATCH = gql`
+	mutation addWatch($videoId: String!, $title: String!, $thumbnail: String!) {
+		addWatch(videoId: $videoId, title: $title, thumbnail: $thumbnail) {
+			id
+			title
+			thumbnail
+		}
+	}
+`;
