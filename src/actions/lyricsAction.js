2554fec5ef0ea9c732d13396e2aab35bbf961c0c
unknown <mehedi5051@gmail.com>
2020-05-27 12:45:27 +0600

    if artist or title not provided then not trying to fetch lyrics

--- src/actions/lyricsAction.js
+++ src/actions/lyricsAction.js
@@ -1,9 +1,14 @@
 import axios from "axios";
-import { GET_LYRICS } from "./types";
+import { GET_LYRICS, CLEAR_LYRICS } from "./types";
 
 export const getLyricsAction = (title) => async (dispatch) => {
 	const artist = title.split("-")[0];
 	const splittedTitle = title.split("-")[1];
+	if (!artist || !splittedTitle) {
+		dispatch({ type: CLEAR_LYRICS });
+		return;
+	}
+
 	const url = `https://api.lyrics.ovh/v1/${artist}/${splittedTitle}`;
 	const res = await axios.get(url);
 
